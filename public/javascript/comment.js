ecd226ba42abbe02e48669235ed87ff6281ea64b
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-05-30 10:25:21 -0400

    js for edit/delete posts

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -2,7 +2,6 @@ async function commentFormHandler(event) {
   event.preventDefault();
 
   const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
-
   const post_id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 1
   ];
@@ -18,7 +17,7 @@ async function commentFormHandler(event) {
         'Content-Type': 'application/json'
       }
     });
-  
+
     if (response.ok) {
       document.location.reload();
     } else {
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,3 +1,27 @@
+async function loginFormHandler(event) {
+  event.preventDefault();
+
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'post',
+      body: JSON.stringify({
+        email,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
 async function signupFormHandler(event) {
   event.preventDefault();
 
@@ -16,33 +40,8 @@ async function signupFormHandler(event) {
       headers: { 'Content-Type': 'application/json' }
     });
 
-    // check the response status
     if (response.ok) {
-      console.log('success');
-    } else {
-      alert(response.statusText);
-    }
-  }
-}
-
-async function loginFormHandler(event) {
-  event.preventDefault();
-
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
-
-  if (email && password) {
-    const response = await fetch('/api/users/login', {
-      method: 'post',
-      body: JSON.stringify({
-        email,
-        password
-      }),
-      headers: { 'Content-Type': 'application/json' }
-    });
-
-    if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('/dashboard/');
     } else {
       alert(response.statusText);
     }
@@ -50,3 +49,5 @@ async function loginFormHandler(event) {
 }
 
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
