e5cba640afd5175b4881fb2f525d6b1706e79bd2
Lucas Pittam <98059989+lucaspittam@users.noreply.github.com>
2022-05-31 10:06:58 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = require('./controllers');
-//const sequelize = require('./config/connection');
+const sequelize = require('./config/connection');
 const path = require('path');
 
 
