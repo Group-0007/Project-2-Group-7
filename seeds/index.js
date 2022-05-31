d59ce1f7ff3c409e8b17f947dcf410d68f199b8f
David Sklar <davidhsklar@gmail.com>
2022-05-31 19:59:40 -0400

    updated seed files and packages

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,3 @@
-
-const seedPosts = require('./post-seeds');
 const seedComments = require('./comment-seeds');
 const seedVotes = require('./vote-seeds');
 const sequelize = require('../config/connection');
@@ -7,9 +5,7 @@ const sequelize = require('../config/connection');
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('--------------');
-  await seedPosts();
-  console.log('--------------');
-
+  
   await seedComments();
   console.log('--------------');
 
