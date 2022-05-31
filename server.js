79f834cd5268be407a7d34d5e606778235c7aa14
Lucas Pittam <98059989+lucaspittam@users.noreply.github.com>
2022-05-31 10:34:30 -0400

    edited code on connection.js and server.js

--- server.js
+++ server.js
@@ -2,7 +2,23 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
+const helpers = require('./utils/help');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({helpers});
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+require('dotenv').config();
 
+//cookie timeout is 1 hour
+const sess = {
+    secret: 'process.env.DB_SECRET',
+    cookie: {maxAge: 3600000},
+    resave: false,
+    saveUninitialized: false,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 
 const app = express();
@@ -10,12 +26,16 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
+//handlebar
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
-//turn on routes
 app.use(routes);
 
-//turn on connection to db and server
+//Connection to server and database
 sequelize.sync({force: false}).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
