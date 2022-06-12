81c6d8de92399f8bf090a5c92551ab3f1985d719
Lucas Pittam <98059989+lucaspittam@users.noreply.github.com>
2022-06-12 11:12:20 -0400

    real estate api

--- server.js
+++ server.js
@@ -10,8 +10,21 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 require('dotenv').config();
 var morgan = require('morgan')
 const fetch = require('node-fetch');
+const url = 'https://realty-in-ca1.p.rapidapi.com/properties/list-residential?LatitudeMax=81.14747595814636&LatitudeMin=-22.26872153207163&LongitudeMax=-10.267941690981388&LongitudeMin=-136.83037765324116&CurrentPage=1&RecordsPerPage=10&SortOrder=A&SortBy=1&CultureId=1&NumberOfDays=0&BedRange=0-0&BathRange=0-0&RentMin=0';
 
-const url = 'https://realty-in-ca1.p.rapidapi.com/locations/auto-complete?Area=Ontario&CultureId=1';
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'realty-in-ca1.p.rapidapi.com',
+    'X-RapidAPI-Key': '6f5f17331cmshc434ece2ea4ac7ap1b6b51jsn2db748cde5ea'
+  }
+};
+
+fetch(url, options)
+	.then(res => res.json())
+	.then(json => console.log(json))
+	.catch(err => console.error('error:' + err))
+
 
 //cookie timeout is 1 hour
 const sess = {
@@ -40,18 +53,6 @@ app.set('view engine', 'handlebars');
 
 app.use(routes);
 
-const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Host': 'realty-in-ca1.p.rapidapi.com',
-      'X-RapidAPI-Key': '6f5f17331cmshc434ece2ea4ac7ap1b6b51jsn2db748cde5ea'
-    }
-  };
-  
-  fetch(url, options)
-      .then(res => res.json())
-      .then(json => console.log(json))
-      .catch(err => console.error('error:' + err));
 
 //Connection to server and database
 sequelize.sync({force: false}).then(() => {
